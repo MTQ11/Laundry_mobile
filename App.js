d002efd75b69781bb08b98e91634c768090795c7
MTQ <mtquyen1002@gmail.com>
2023-08-24 17:37:42 +0700

    add screen, component

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Home/>
       <Text>Hello World !!!</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
